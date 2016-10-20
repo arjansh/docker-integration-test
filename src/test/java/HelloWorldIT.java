4ac173ef9d32bbce9039aaaa25359096edee14bd
Arjan Seijkens <Arjan.Seijkens@humaninference.com>
2016-10-20 16:18:49 +0200

    Fallback to "localhost" if no value can be retrieved for DOCKER_HOST environment variable.

--- src/test/java/HelloWorldIT.java
+++ src/test/java/HelloWorldIT.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Properties;
 
@@ -11,13 +12,15 @@ import org.junit.Test;
 
 public class HelloWorldIT {
 
+    private static final String DEFAULT_DOCKER_HOST_NAME = "localhost";
+
     @Test
     public void testHelloWorld() throws Exception {
         final Properties testProperties = new Properties();
         testProperties.load(getClass().getClassLoader().getResourceAsStream("test.properties"));
 
-        final URL testURL = new URL("http://" + (new URI(System.getenv("DOCKER_HOST"))).getHost() + ":" + testProperties
-                .getProperty("portnumber") + "/" + testProperties.getProperty("contextpath"));
+        final URL testURL = new URL("http://" + determineHostName() + ":" + testProperties.getProperty("portnumber")
+                + "/" + testProperties.getProperty("contextpath"));
         final HttpURLConnection connection = (HttpURLConnection) testURL.openConnection();
         connection.setRequestMethod("GET");
 
@@ -28,5 +31,16 @@ public class HelloWorldIT {
         inputReader.close();
 
         assertTrue(response.toString().contains("Hello world!"));
+    }
+
+    private static String determineHostName() throws URISyntaxException {
+        final String dockerHost = System.getenv("DOCKER_HOST");
+
+        if (dockerHost == null) {
+            // If no value is returned for the DOCKER_HOST environment variable fall back to a default.
+            return DEFAULT_DOCKER_HOST_NAME;
+        } else {
+            return (new URI(dockerHost)).getHost();
+        }
     }
 }
