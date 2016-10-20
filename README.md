b4f929c79568af2d97c6fb5bc2ec6630ac48f78b
Arjan Seijkens <Arjan.Seijkens@humaninference.com>
2016-10-20 16:23:49 +0200

    Remove docker host name env variable (#4)
    * Removed usage of DOCKER_HOST_NAME environment variable. Deducing the DOCKER_HOST_NAME from the DOCKER_HOST instead.
    
    * Trying a bit of a different approach.
    
    * Revert "Trying a bit of a different approach."
    
    This reverts commit af90ba13c5c0cbde86baf84cb6670cc5169885cb.
    
    * Fallback to "localhost" if no value can be retrieved for DOCKER_HOST environment variable.

--- README.md
+++ README.md
@@ -10,16 +10,15 @@ To be able to run the project you need to have the following installed:
  * Maven 3
  * Docker
  
-Furthermore the following environment variables have to be set:
+Furthermore, depending on your setup the following environment variables may have to be set:
 
- * DOCKER_HOST_NAME - contains the host name of your Docker Daemon, e.g. localhost or 192.168.99.100.
  * DOCKER_HOST - contains the URL of the Docker Daemon, e.g. http://localhost:2375 or tcp://192.168.99.1000:2376.
  * DOCKER_CERT_PATH - contains the path to the SSL certificate when SSL is used for communicating with the Docker daemon, e.g. ~/.docker/ or C:\Users\\[username]\\.docker\machine\certs.
  
-The DOCKER_HOST_NAME have to be set in all use cases. DOCKER_HOST and DOCKER_CERT_PATH may not be necessary depending on your setup.
-
 Now you can run the project using
 
 ```
 mvn clean verify
 ```
+
+By default it starts tomcat on port 8080, you can override this using the dockerTomcatPort maven property if you want to run it on another port, e.g. `mvn clean verify -PdockerTomcatPort=8081`.
--- src/test/java/HelloWorldIT.java
+++ src/test/java/HelloWorldIT.java
@@ -1,9 +1,10 @@
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Properties;
 
@@ -11,13 +12,15 @@ import org.junit.Test;
 
 public class HelloWorldIT {
 
-    @Test
-    public void testHelloWorld() throws IOException {
+    private static final String DEFAULT_DOCKER_HOST_NAME = "localhost";
 
+    @Test
+    public void testHelloWorld() throws Exception {
         final Properties testProperties = new Properties();
         testProperties.load(getClass().getClassLoader().getResourceAsStream("test.properties"));
 
-        final URL testURL = new URL(testProperties.getProperty("testurl"));
+        final URL testURL = new URL("http://" + determineHostName() + ":" + testProperties.getProperty("portnumber")
+                + "/" + testProperties.getProperty("contextpath"));
         final HttpURLConnection connection = (HttpURLConnection) testURL.openConnection();
         connection.setRequestMethod("GET");
 
@@ -28,5 +31,16 @@ public class HelloWorldIT {
         inputReader.close();
 
         assertTrue(response.toString().contains("Hello world!"));
+    }
+
+    private static String determineHostName() throws URISyntaxException {
+        final String dockerHost = System.getenv("DOCKER_HOST");
+
+        if (dockerHost == null) {
+            // If no value is returned for the DOCKER_HOST environment variable fall back to a default.
+            return DEFAULT_DOCKER_HOST_NAME;
+        } else {
+            return (new URI(dockerHost)).getHost();
+        }
     }
 }
